cd8b52eee46d125176f486f863095797f28d728b
Chattiori <127287320+Faildes@users.noreply.github.com>
2024-03-31 14:51:45 +0900

    Update lora_conversion.py

--- lora_conversion.py
+++ lora_conversion.py
@@ -3,10 +3,9 @@ import datetime
 
 from scripts.inference_lora import lora_c
 
-def lora_convert(lora_path, output_path):
+def lora_convert(lora_path, adapter, output_path):
     base = "runwayml/stable-diffusion-v1-5"
     sdxl = "stabilityai/stable-diffusion-xl-base-1.0"
     vae = "madebyollin/sdxl-vae-fp16-fix"
-    adapter = "./checkpoint/X-Adapter/X_Adapter_v1.bin"
     lora_c(lora_path, base, sdxl, vae, adapter, output_path)
     print("Done!")
